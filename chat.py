4bf86f919c6812b636bcbf18a075bc2e90fadd7c
meotttt <Ananasblat@gmail.com>
2025-12-17 17:09:41 +0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -89,8 +89,8 @@ ACHIEVEMENTS = [
 # Короткий откат при использовании крутки (в секундах)
 SPIN_USED_COOLDOWN = 600  # 10 минут
 REPEAT_CRYSTALS_BONUS = 80  # Кристаллы за повторную карточку
-COLLECTION_MENU_IMAGE_PATH = os.path.join(PHOTO_BASE_PATH, "photo_2025-12-17_17-03-14.jpg")
-NOTEBOOK_MENU_IMAGE_PATH = os.path.join(PHOTO_BASE_PATH, "photo_2025-12-17_17-01-44.jpg")
+COLLECTION_MENU_IMAGE_PATH = os.path.join(PHOTO_BASE_PATH, "photo_2025-12-17_17-01-44.jpg")
+NOTEBOOK_MENU_IMAGE_PATH = os.path.join(PHOTO_BASE_PATH, "photo_2025-12-17_17-03-14.jpg")
 
 # --- Конфигурация из второго скрипта (Брак, Админ, Евангелие) ---
 REUNION_PERIOD_DAYS = 3  # Количество дней для льготного периода после развода
@@ -3753,6 +3753,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
